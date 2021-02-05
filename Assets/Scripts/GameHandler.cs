dbc51728aaebc56827974849d20c7416d49dbccb
Olivia Schirm <oliviaschirm@schirm.local>
2021-02-04 22:05:07 -0500

    health bar

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour{
+
+	public HealthBar healthBar;
+    
+    private void Start(){
+    	HealthSystem healthSystem = new HealthSystem(100);
+        Debug.Log(healthBar);
+    	healthBar.Setup(healthSystem);
+
+    	Debug.Log("Health: " + healthSystem.GetHealthPercent());
+    	healthSystem.Damage(10);
+    	Debug.Log("Health: " + healthSystem.GetHealthPercent());
+		/*healthSystem.Heal(10);
+    	Debug.Log("Health: " + healthSystem.GetHealthPercent());*/
+    }
+}
