d8c71c835bee1cc33ef64483611088044255cc5e
Anna Arpaci-Dusseau <anna.arpacidusseau@gmail.com>
2021-02-05 19:45:23 -0500

    Hunger and Tiredness

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,22 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
-	public int health = 20;
 	public int hunger = 10;
-	public int cold = 10;
+	public int tired = 10;
 
-    public bool warm = false;
+    public HealthSystem health;
+
 
 	public float hunger_counter;
-    public float cold_counter;
+    public float tired_counter;
+
+    public Text HungerText;
+    public Text TiredText;
 
     void Update()
 	{
 		hunger_counter += Time.deltaTime;
-        cold_counter += Time.deltaTime;
+        tired_counter += Time.deltaTime;
 
         if(hunger_counter > 20)
 		{
@@ -24,30 +28,28 @@ public class PlayerStats : MonoBehaviour
 			hunger_counter = 0;
             if (hunger < 1)
             {
-                health--;
+                health.Damage(1);
             }
         }
 
-        if(cold_counter > 10)
+        if(tired_counter > 10)
         {
-            if (warm)
-            {
-                cold += 2;
-                cold_counter = 0;
-            }
-            cold -= 1;
-            cold_counter = 0;
+            tired -= 1;
+            tired_counter = 0;
 
-            if(cold < 1)
+            if(tired < 1)
             {
-                health--;
+                health.Damage(1);
             }
         }
 
-        if(cold > 10) { cold = 10; }
+        if(tired > 10) { tired = 10; }
         if(hunger > 10) { hunger = 10; }
-        if(cold < 0) { cold = 0; }
+        if(tired < 0) { tired = 0; }
         if (hunger < 0) { hunger = 0; }
+
+        HungerText.text = "Hunger: " + hunger;
+        TiredText.text = "Tired: " + tired;
 
     }
 
@@ -55,7 +57,7 @@ public class PlayerStats : MonoBehaviour
     {
         if (name == "health")
         {
-            return health / 20f;
+            return health.GetHealthPercent();
         }
 
         if(name == "hunger")
@@ -63,9 +65,9 @@ public class PlayerStats : MonoBehaviour
             return hunger / 10f;
         }
 
-        if (name == "cold")
+        if (name == "tired")
         {
-            return cold / 10f;
+            return tired / 10f;
         }
 
         return -1;
