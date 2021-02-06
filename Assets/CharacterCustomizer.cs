9543f9dd4de7be9a7e4f6969060be199b3c7af55
Anna Arpaci-Dusseau <anna.arpacidusseau@gmail.com>
2021-02-06 13:46:20 -0500

    color customizer in character creator

---
+++ Assets/CharacterCustomizer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCustomizer : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] players;
+    private int playerIndex = 0;
+    private GameObject player;
+    [SerializeField] private GameObject uiPanel;
+    [SerializeField] private GameObject slider;
+    [SerializeField] private List<GameObject> oldList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        oldList = new List<GameObject>();
+        CreateMaterialCustomizer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateMaterialCustomizer()
+    {
+
+        Debug.Log(oldList.Count);
+        foreach (GameObject old in oldList)
+        {
+            Destroy(old);
+        }
+        oldList.Clear();
+
+
+        Material[] mats = player.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().materials;
+        int yOff = 0;
+        foreach(Material m in mats)
+        {
+            Debug.Log(m.name + " " + m.color);
+            GameObject s = Instantiate(slider, uiPanel.transform) as GameObject;
+            s.active = true;
+            s.transform.position = s.transform.position + new Vector3(0,yOff, 0);
+            s.GetComponent<ColorSelector>().setMaterial(m);
+            s.GetComponent<ColorSelector>().setColor(m.color);
+            s.GetComponent<ColorSelector>().setText(m.name.Split()[0]);
+            yOff -= 60;
+            oldList.Add(s);
+        }
+    }
+
+    public void OnValueChanged(float test)
+    {
+        GameObject oldPlayer = player;
+        playerIndex = Mathf.RoundToInt(test);
+        Debug.Log(players[playerIndex]);
+        player = Instantiate(players[playerIndex], oldPlayer.transform.position, Quaternion.identity) as GameObject;
+        player.name = "Player";
+        Destroy(oldPlayer);
+        CreateMaterialCustomizer();
+    }
+}
---
+++ Assets/ColorSelector.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorSelector : MonoBehaviour
+{
+
+    [SerializeField] private Slider rSlider;
+    [SerializeField] private Slider gSlider;
+    [SerializeField] private Slider bSlider;
+
+    [SerializeField] private Text text;
+
+
+
+
+    public Color color;
+    private Material m;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rSlider.onValueChanged.AddListener(delegate { updateRed(); }) ;
+        gSlider.onValueChanged.AddListener(delegate { updateGreen(); });
+        bSlider.onValueChanged.AddListener(delegate { updateBlue(); });
+
+    }
+
+
+    public void updateRed()
+    {
+        color.r = rSlider.value;
+        m.color = color;
+
+    }
+
+    public void updateGreen()
+    {
+        color.g = gSlider.value;
+        m.color = color;
+    }
+
+    public void setMaterial(Material mat)
+    {
+        m = mat;
+    }
+    public void setText(string s)
+    {
+        text.text = s;
+    }
+    public void updateBlue()
+    {
+        color.b = bSlider.value;
+        m.color = color;
+
+    }
+    public void setColor(Color c)
+    {
+        color = c;
+        rSlider.value = c.r;
+        gSlider.value = c.g;
+        bSlider.value = c.b;
+    }
+}
