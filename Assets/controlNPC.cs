c470989de9027b8ae163ec4ce0c688f053bb6bb3
Sofia <>
2021-02-06 13:48:56 -0500

    changes to npc movement

--- Assets/controlNPC.cs
+++ Assets/controlNPC.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class controlNPC : MonoBehaviour
 {
+    public Animator animator;
+    public Vector3 target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      if(Input.GetKeyDown(KeyCode.K)){
+        animator.SetTrigger("walk");
+
+        }
     }
+
+
+
 }
