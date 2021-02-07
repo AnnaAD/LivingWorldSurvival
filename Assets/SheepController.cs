67f6b769623998941cc0040c0918b44384b73480
Anna Arpaci-Dusseau <anna.arpacidusseau@gmail.com>
2021-02-07 17:35:56 -0500

    adding sheep movement

--- Assets/SheepController.cs
+++ Assets/SheepController.cs
@@ -6,11 +6,18 @@ public class SheepController : MonoBehaviour
 {
     private HealthSystem health;
     [SerializeField] private GameObject drop;
+    private Vector2 targetDestination;
+    private Rigidbody rb;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        health = new HealthSystem(20);
+        health = new HealthSystem(4);
         transform.Find("HealthBar").GetComponent<HealthBar>().Setup(health);
+        targetDestination = new Vector2(transform.position.x, transform.position.z);
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
@@ -23,6 +30,23 @@ public class SheepController : MonoBehaviour
             Destroy(this.gameObject);
             Instantiate(drop, transform.position, Quaternion.identity);
         }
+
+        if(Vector2.Distance(new Vector2(transform.position.x, transform.position.z), targetDestination) < 1f)
+        {
+            Vector2 newPosOffset = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+            targetDestination = targetDestination + newPosOffset;
+        }
+
+       
+
+        transform.LookAt(new Vector3(targetDestination.x, transform.position.y, targetDestination.y));
+
+        rb.velocity = transform.forward * 3f;
+
+        animator.SetFloat("Speed", rb.velocity.magnitude);
+
+
+
     }
 
     public void OnTriggerEnter(Collider other)
@@ -30,6 +54,8 @@ public class SheepController : MonoBehaviour
         if(other.gameObject.tag == "Damage")
         {
             health.Damage(other.gameObject.GetComponent<DamageStats>().damage);
+            Vector3 moveDirection = rb.transform.position - other.transform.position;
+            rb.AddForce(moveDirection.normalized * -500f);
         }
     }
 }
