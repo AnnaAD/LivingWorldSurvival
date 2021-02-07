cd1cefaaa6aa5b8f9f96d35b46c5471956fc5ad7
Anna Arpaci-Dusseau <anna.arpacidusseau@gmail.com>
2021-02-07 15:20:11 -0500

    added sheep and killing the sheep

---
+++ Assets/SheepController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepController : MonoBehaviour
+{
+    private HealthSystem health;
+    private GameObject drop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = new HealthSystem(20);
+        transform.Find("HealthBar").GetComponent<HealthBar>().Setup(health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(health.GetHealth());
+        if(health.GetHealth() <= 0)
+        {
+            Debug.Log("die");
+            Destroy(this.gameObject);
+            Instantiate(drop, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Damage")
+        {
+            health.Damage(other.gameObject.GetComponent<DamageStats>().damage);
+        }
+    }
+}
