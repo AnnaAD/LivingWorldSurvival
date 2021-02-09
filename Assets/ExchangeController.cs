5905821a2f81bca04077e5ef9b6e60058e9a9173
Anna Arpaci-Dusseau <anna.arpacidusseau@gmail.com>
2021-02-09 16:42:05 -0500

    exchange menu targets different npcs

--- Assets/ExchangeController.cs
+++ Assets/ExchangeController.cs
@@ -7,11 +7,16 @@ public class ExchangeController : MonoBehaviour
     [SerializeField] private GameObject player_inventory;
     [SerializeField] private GameObject npc_inventory;
     private bool toggleInventory;
+    private Inventory pInventory;
+    private Inventory npcInventory;
     // Start is called before the first frame update
     void Start()
     {
         toggleInventory = false;
+        pInventory = GameObject.Find("Player").GetComponent<PlayerPickup>().inventory;
+        npcInventory = null;
     }
+    
 
     // Update is called once per frame
     void Update()
@@ -19,6 +24,29 @@ public class ExchangeController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
         {
             toggleInventory = !toggleInventory;
+            Vector3 p1 = GameObject.Find("Player").transform.position;
+            float distanceToObstacle = 0;
+            RaycastHit[] hit = Physics.SphereCastAll(p1, 20, GameObject.Find("Player").transform.forward, 20);
+
+
+     
+            if (toggleInventory && hit.Length > 0)
+            {
+                foreach ( RaycastHit r in hit)
+                {
+                    if(r.collider.tag == "NPC")
+                    {
+                        Debug.Log("found NPC");
+                        npcInventory = r.collider.gameObject.GetComponent<PlayerPickup>().inventory;
+                    }
+                }
+            }
+
+            if(npcInventory == null)
+            {
+                toggleInventory = false;
+            }
+
             if (!toggleInventory)
             {
 
@@ -30,8 +58,9 @@ public class ExchangeController : MonoBehaviour
             {
                 Cursor.visible = true;
                 Cursor.lockState = CursorLockMode.None;
-                npc_inventory.GetComponent<UI_Inventory>().setInventory(GameObject.Find("NPC").GetComponent<PlayerPickup>().inventory);
-                player_inventory.GetComponent<UI_Inventory>().setInventory(GameObject.Find("Player").GetComponent<PlayerPickup>().inventory);
+
+                npc_inventory.GetComponent<UI_Inventory>().setInventory(npcInventory);
+                player_inventory.GetComponent<UI_Inventory>().setInventory(pInventory);
             }
         }
         player_inventory.gameObject.SetActive(toggleInventory);
@@ -40,5 +69,10 @@ public class ExchangeController : MonoBehaviour
         
 
 
+    }
+    public void updateInventories(Inventory player_inv, Inventory npc_inv)
+    {
+        pInventory = player_inv;
+        npcInventory = npc_inv;
     }
 }
