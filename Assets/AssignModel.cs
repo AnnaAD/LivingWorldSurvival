8150166eb30260fdb2b52fe9356785674f1e69a5
Anna Arpaci-Dusseau <anna.arpacidusseau@gmail.com>
2021-02-10 20:51:43 -0500

    Added Flexible Model Player/NPC prefabs.
    Script assigns the models, animation armatures and avatars.

---
+++ Assets/AssignModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignModel : MonoBehaviour
+{
+    public GameObject[] playerModels;
+    public Avatar[] playerAvatars;
+    public int modelId;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.position);
+        GameObject model = Instantiate(playerModels[modelId],transform);
+
+        model.transform.position = transform.position;
+
+        model.transform.GetChild(0).gameObject.AddComponent<RandomColor>();
+
+        GetComponent<Animator>().avatar = playerAvatars[modelId];
+
+    }
+}
